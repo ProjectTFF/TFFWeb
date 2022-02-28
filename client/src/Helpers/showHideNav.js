f0fdafdcbff79906b78df82db974402aac49a7e7
Samuel Goncalves <samuel.goncalves@epita.fr>
2022-02-28 12:27:58 +0200

    correct showHideNav + client skeleton (2)

--- client/src/Helpers/showHideNav.js
+++ client/src/Helpers/showHideNav.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import NavMenu from '../Components/navMenu';
-import Burger from '../Assets/menu.svg'
+import Burger from '../Assets/Images/menu.svg';
 
 const Right = {
-    display : "flex",
-    padding : "0.5em",
-    position : "absolute",
-    right : "0",
-}
+  display: 'flex',
+  padding: '0.5em',
+  position: 'absolute',
+  right: '0',
+};
 
 function ShowHideNav() {
-    const [show,setShow]=React.useState(false)
-    return (
-        <div className='ShowHideNav'>
-            <button style={Right} onClick={()=>setShow(!show)}>
-                <img src={Burger}/>
-            </button>
-            
-            {
-                show?<NavMenu />
-                :null
-            }
-            
-        </div>
-    )
+  const [show, setShow] = React.useState(false);
+  return (
+    <div className="ShowHideNav">
+      <button style={Right} onClick={() => setShow(!show)} type="button">
+        <img src={Burger} alt="Navigation Button" />
+      </button>
+      {
+        show ? <NavMenu />
+        : null
+      }
+    </div>
+  );
 }
 
 export default ShowHideNav;
