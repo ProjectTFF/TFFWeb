db69aa7085d2f34bc0e2e0ab14775783b84dfc49
Eliana Junker <eliana.junker@tuni.fi>
2022-04-01 11:01:05 +0300

    add the text to explain how to bookmark

--- client/src/Components/modal.jsx
+++ client/src/Components/modal.jsx
@@ -35,9 +35,46 @@ function Modal(props) {
           <CancelIcon />
         </button>
 
-        <span className="modal-title">Bookmark Information</span>
+        <span className="modal-title">How to bookmark</span>
         <div className="modal-body">
-          <p>Here the information for how to use bookmark option in mobile will added.</p>
+          <p>Here is how to place a bookmark of our website on your home screen:</p>
+          <br />
+          <p>
+            For Iphone :
+            <ol>
+              <li>open the website in the Safari app</li>
+              <li>
+                tap the share icon (an icon featuring a right-pointing
+                arrow coming out of a box) and then select “Add to Home Screen”
+              </li>
+              <li>
+                type the name you want for the bookmark and then click on “Add”.
+                It will then appear on your home screen !
+              </li>
+            </ol>
+          </p>
+          <p>
+            For Androids :
+            <ol>
+              <li>open our website with Chrome or Firefox</li>
+              <li>
+                tap the menu icon (3 dots in upper right-hand corner) and tap “Add to homescreen”.
+              </li>
+              <li>
+                enter the name you want for the bookmark (or keep the default one) and click on
+                “Add”.It will then appear on your home screen !
+              </li>
+            </ol>
+            Or
+            <ol>
+              <li>open the website with Samsung internet</li>
+              <li>tap the three-lines menu (generally at the bottom-right corner of the screen)</li>
+              <li>
+                tap on the choice “Add page to” and then on the “home screen” option.
+                Then, click on “Add” and the website will appear on your home screen !
+              </li>
+            </ol>
+          </p>
         </div>
       </div>
     </div>
