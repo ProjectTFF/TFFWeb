98fa40c7a9cd4ae12e3e5b6672013b9e3a3fa407
Samuel Goncalves <samuel.goncalves@epita.fr>
2022-04-15 23:56:10 +0300

    Programme page (2)

--- client/src/Pages/programme.jsx
+++ client/src/Pages/programme.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import '../Assets/Styles/programmePage.css';
 import Banner from '../Components/banner';
 import PrimaryButton from '../Components/primaryButton';
-import DefaultButton from '../Components/defaultButton';
 
 function Programme(props) {
   const {
     language, handleSetLanguage,
    } = props;
+
+  /**
+   * Get information from backend (All programmes)
+   */
+  const [programmes, setProgrammes] = React.useState([]);
+  if (programmes.length === 0) {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/programme/allprogrammes`).then((res) => { const val = res.data; setProgrammes(val); });
+  }
+
+  const [timetable, setTimetable] = React.useState([]);
+
+  if (programmes.length !== 0 && timetable.length === 0) {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/programme/1`).then((res) => { const val = res.data; setTimetable(val); });
+  }
 
   // Content of the page by language
   let content = {
@@ -15,7 +29,6 @@ function Programme(props) {
       programmeTitle: 'PROGRAMME',
       bannerTitle: 'Tampere Flute Fest',
       hall: 'Tampere Hall',
-      soon: 'Coming Soon!',
       buyTickets: 'buy tickets',
       buyStreaming: 'buy Streaming',
       ticketLink: 'https://www.lippu.fi/en/artist/tampere-flute-fest/',
@@ -25,7 +38,6 @@ function Programme(props) {
       programmeTitle: 'PROGRAMME (Finnish)',
       bannerTitle: 'Tampere Flute Fest',
       hall: 'Tampere-Talo',
-      soon: 'Tulossa!',
       buyTickets: 'osta liput',
       buyStreaming: 'osta suoratoisto',
       ticketLink: 'https://www.lippu.fi/artist/tampere-flute-fest/',
@@ -45,209 +57,36 @@ function Programme(props) {
       <div className="programme-holder">
         <div className="container">
           <h1 className="page-title">{content.programmeTitle}</h1>
-          <div className="pass-division">
-            <h2 className="pass-title">
-              24.4.22 NAME OF THE PASS
-              <ul className="btn-near">
-                <li>
-                  <PrimaryButton
-                    url={content.ticketLink}
-                    buttonText={content.buyTickets}
-                    showIcon
-                  />
-                </li>
-                <li>
-                  <DefaultButton
-                    url={content.streamLink}
-                    buttonText={content.buyStreaming}
-                    showIcon
-                  />
-                </li>
-              </ul>
-            </h2>
-            <p className="pass-infos">
-              <p>description</p>
-              <p>
-                {'Location : '}
-                <span>here</span>
+          {programmes.map((programme) => (
+            <div className="pass-division">
+              <h2 className="pass-title">
+                {`${programme.concertdate} ${programme.concertname}`}
+                <ul className="btn-near">
+                  <li>
+                    <PrimaryButton
+                      url={content.ticketLink}
+                      buttonText={content.buyTickets}
+                      showIcon
+                    />
+                  </li>
+                </ul>
+              </h2>
+              <p className="pass-infos">
+                {/* <p>description</p> */}
+                <p>
+                  {'Location : '}
+                  <span>{programme.venuename}</span>
+                </p>
+                {/* <p className="pass-special">special info</p> */}
               </p>
-              <p className="pass-special">special info</p>
-            </p>
-            <ol className="pass-timetable">
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-            </ol>
-          </div>
-          <div className="pass-division">
-            <h2 className="pass-title">
-              24.4.22 NAME OF THE PASS
-              <ul className="btn-near">
-                <li>
-                  <PrimaryButton
-                    url={content.ticketLink}
-                    buttonText={content.buyTickets}
-                    showIcon
-                  />
-                </li>
-                <li>
-                  <DefaultButton
-                    url={content.streamLink}
-                    buttonText={content.buyStreaming}
-                    showIcon
-                  />
-                </li>
-              </ul>
-            </h2>
-            <p className="pass-infos">
-              <p>description</p>
-              <p>
-                {'Location : '}
-                <span>here</span>
-              </p>
-              <p className="pass-special">special info</p>
-            </p>
-            <ol className="pass-timetable">
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-            </ol>
-          </div>
-          <div className="pass-division">
-            <h2 className="pass-title">
-              24.4.22 NAME OF THE PASS
-              <ul className="btn-near">
-                <li>
-                  <PrimaryButton
-                    url={content.ticketLink}
-                    buttonText={content.buyTickets}
-                    showIcon
-                  />
-                </li>
-                <li>
-                  <DefaultButton
-                    url={content.streamLink}
-                    buttonText={content.buyStreaming}
-                    showIcon
-                  />
-                </li>
-              </ul>
-            </h2>
-            <p className="pass-infos">
-              <p>description</p>
-              <p>
-                {'Location : '}
-                <span>here</span>
-              </p>
-              <p className="pass-special">special info</p>
-            </p>
-            <ol className="pass-timetable">
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-              <li>
-                <p className="pass-time">time</p>
-                <p className="pass-right-timetable">
-                  <p className="pass-event">event</p>
-                  <p className="pass-artists">
-                    {'artists: '}
-                    <span>
-                      name
-                    </span>
-                  </p>
-                </p>
-              </li>
-            </ol>
-          </div>
+              <ol className="pass-timetable">
+                {timetable.map((event) => (
+                  event.performancename_eng
+                ))}
+              </ol>
+            </div>
+         ))}
         </div>
-        <hr />
       </div>
     </>
   );
